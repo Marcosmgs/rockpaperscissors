9ee393b5d57e727bab6c5216fcb15630b3a9f9a2
marcosmgs <marcosmgs2023@gmail.com>
2022-11-18 19:48:41 +0000

    Initial js script commit, add main game elements to the DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log("I am working!!!");
+const startBtn = document.getElementById("start-btn");
+const scoreAreaDiv = document.getElementById("score-area");
+const resultAreaDiv = document.getElementById("results-area");
+const moveCallText = document.getElementById("call-move");
+const buttonsUserOptions = document.getElementById("game-control");
+const resetBtn = document.getElementById("reset-btn");
+
+
+startBtn.addEventListener('click', gameOn);
+
+
+function gameOn() {
+    startBtn.classList.add('hide');
+    scoreAreaDiv.classList.remove('hide');
+    resultAreaDiv.classList.remove('hide');
+    moveCallText.classList.remove('hide');
+    buttonsUserOptions.classList.remove('hide');
+    resetBtn.classList.remove('hide');
+}
